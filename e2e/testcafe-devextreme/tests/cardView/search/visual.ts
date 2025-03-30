49dd4d28789c8901061e456692292ba09ed9ee36
Andrey Dolzhikov <eylau-andrew@mail.ru>
2025-03-30 04:17:05 +0300

    Fix search appearance

--- e2e/testcafe-devextreme/tests/cardView/search/visual.ts
+++ e2e/testcafe-devextreme/tests/cardView/search/visual.ts
@@ -3,43 +3,12 @@ import CardView from 'devextreme-testcafe-models/cardView';
 import url from '../../../helpers/getPageUrl';
 import { createWidget } from '../../../helpers/createWidget';
 import { testScreenshot } from '../../../helpers/themeUtils';
+import { data } from '../helpers/simpleArrayData';
 
-// TODO: Unskip this fixture after markup will be stabilized
 fixture`Search.Visual`
   .page(url(__dirname, '../../container.html'));
 
 const CARD_VIEW_SELECTOR = '#container';
-const DATA = [{
-  id: 1,
-  firstName: 'Darin',
-  lastName: 'Heritege',
-  email: 'dheritege0@jugem.jp',
-  gender: 'Male',
-}, {
-  id: 2,
-  firstName: 'Aeriel',
-  lastName: 'Giggs',
-  email: 'agiggs1@hubpages.com',
-  gender: 'Female',
-}, {
-  id: 3,
-  firstName: 'Theo',
-  lastName: 'Aleksidze',
-  email: 'taleksidze2@patch.com',
-  gender: 'Female',
-}, {
-  id: 4,
-  firstName: 'Dalli',
-  lastName: 'Ashwood',
-  email: 'dashwood3@buzzfeed.com',
-  gender: 'Male',
-}, {
-  id: 5,
-  firstName: 'Paule',
-  lastName: 'Pidgeley',
-  email: 'ppidgeley4@upenn.edu',
-  gender: 'Female',
-}];
 
 test('highlighted search text', async (t) => {
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
@@ -52,9 +21,10 @@ test('highlighted search text', async (t) => {
     .expect(compareResults.isValid())
     .ok(compareResults.errorMessages());
 }).before(async () => createWidget('dxCardView', {
-  dataSource: DATA,
+  dataSource: data,
   searchPanel: {
-    text: 'Female',
+    visible: true,
+    text: 'rt',
   },
   height: 600,
 }));
